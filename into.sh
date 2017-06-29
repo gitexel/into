cf41e9271b28ce0994b9e3642be423e29fb91d27
gitexel <ruexel@mail.ru>
2017-06-29 16:18:45 +0200

    Help function added

--- into.sh
+++ into.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 Sexit(){
 
-  command -v $1 >/dev/null 2>&1 || { echo "Package or command not found. Aborting." >&2; exit 1; }
+  command -v $1 >/dev/null 2>&1 || { echo "Package not found. Aborting." >&2; exit 1; }
   if [ "$1" = "" ]; then
-    echo "Package or command not found. Aborting."
+    echo "Package not found. Aborting."
     exit 1;
   fi
 
 }
 
+Help(){
+
+echo "Should include package name, e.g. into echo"
+echo "If you want to change to another package, type: -to packageName"
+
+}
+
+
+if [ $# -eq 0 ]; then
+Help
+else
 line=$1
 Sexit $line
+fi
 
-while true
+while [[ $# > 0 ]]
 
    do
 
